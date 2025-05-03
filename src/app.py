ed76fbed451b5bac297aab9eb5a6c794e7df6ddf
Clay Lancini <claylanzino@gmail.com>
2025-05-03 17:26:23 -0400

    Delete files from remote repository.

--- src/app.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-# Load environment variables from .env file
-load_dotenv()
-
-# Access environment variables
-my_variable = os.getenv("Host")
-print(my_variable)
